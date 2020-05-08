1e70c9411268ef9db0179e96de48678e9e22ea75
Georgios Kotziabassis <kotziabassis@gmail.com>
2020-05-08 10:00:38 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+  setTimeout(() => {
+    mapboxgl.accessToken =
+      'pk.eyJ1Ijoia290emlhYmFzc2lzIiwiYSI6ImNrODBqNnFxdTAxemozcWxteTd4MHBidzQifQ.64bRovdQ4UMKAkiInypf3w'
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/light-v10?optimize=true',
+      center: [9.431, 48.959],
+      zoom: 14,
+      minZoom: 8,
+      maxZoom: 18,
+    })
+
+    const popup = new mapboxgl.Popup({ offset: 25 }).setText(
+      'Frontida Pflegedienst Backnang'
+    )
+
+    const marker = new mapboxgl.Marker({ color: '#2664ad' })
+      .setLngLat([9.431, 48.959])
+      .setPopup(popup)
+      .addTo(map)
+  }, 2000)
+})
+
+const closeMenu = () => {
+  setTimeout(() => {
+    document.querySelector('#overlay-input').click()
+  }, 200)
+}
